60dda1a0a033eb0e77a848d574c678eb74b603f6
Henrik Pedersen <Henrik.Pedersen.vs@gmail.com>
2023-12-06 11:23:38 +0100

    !

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Toggle navigation for smaller screens
     var navToggle = document.getElementById('navToggle');
     var navList = document.getElementById('navList');
 
     navToggle.addEventListener('click', function() {
         navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    // Highlight the active page in the navigation
+    var navItems = document.querySelectorAll('.nav-list li a');
+    var currentPath = window.location.pathname.split('/').pop(); // Get only the filename
+
+    navItems.forEach(function(item) {
+        if (item.getAttribute('href') === currentPath) {
+            item.classList.add('active');
+        }
     });
 });
